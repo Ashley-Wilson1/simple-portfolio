82576bc1cb6ca99f46f02864607a943654f983a6
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-06-18 20:41:39 +0100

    Completed the projects section

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log("hello world");
+const form = document.querySelector(".ts-contact-form");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
+    var _a, _b, _c;
+    e.preventDefault();
+    console.log("click");
+    const emailInput = (_a = document.getElementById("ts-email-input")) === null || _a === void 0 ? void 0 : _a.value.trim();
+    const subjectInput = (_b = document.getElementById("ts-subject-input")) === null || _b === void 0 ? void 0 : _b.value.trim();
+    const bodyInput = (_c = document.getElementById("ts-body-input")) === null || _c === void 0 ? void 0 : _c.value.trim();
+    if (!emailInput || !subjectInput || !bodyInput) {
+        alert("Please fill in all fields");
+    }
+    else {
+        fetch("https://formspree.io/f/xblykqdp", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: emailInput,
+                subject: subjectInput,
+                message: bodyInput,
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                alert("Message sent successfully!");
+            }
+            else {
+                alert("Something went wrong.");
+            }
+        });
+        document.getElementById("ts-email-input").value = "";
+        document.getElementById("ts-subject-input").value =
+            "";
+        document.getElementById("ts-body-input").value = "";
+    }
+});
--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,1 +1,47 @@
-console.log("hello world");
+const form = document.querySelector(".ts-contact-form");
+form?.addEventListener("submit", (e) => {
+	e.preventDefault();
+	console.log("click");
+
+	const emailInput = (
+		document.getElementById("ts-email-input") as HTMLInputElement
+	)?.value.trim();
+	const subjectInput = (
+		document.getElementById("ts-subject-input") as HTMLInputElement
+	)?.value.trim();
+	const bodyInput = (
+		document.getElementById("ts-body-input") as HTMLInputElement
+	)?.value.trim();
+
+	if (!emailInput || !subjectInput || !bodyInput) {
+		alert("Please fill in all fields");
+	} else {
+		fetch("https://formspree.io/f/xblykqdp", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				email: emailInput,
+				subject: subjectInput,
+				message: bodyInput,
+			}),
+		}).then((res) => {
+			if (res.ok) {
+				alert("Message sent successfully!");
+			} else {
+				alert("Something went wrong.");
+			}
+		});
+
+		(document.getElementById("ts-email-input") as HTMLInputElement).value = "";
+		(document.getElementById("ts-subject-input") as HTMLInputElement).value =
+			"";
+		(document.getElementById("ts-body-input") as HTMLInputElement).value = "";
+	}
+});
+
+interface IProject {
+	name: string;
+	summary: string;
+	image: string;
+	icons: string[];
+}
